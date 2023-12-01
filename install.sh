c8c073a742848ffb63de369a3d4c7323ca49d64f
luiscbedoya <bedoyaandocillaluiscarlos@gmail.com>
2023-12-01 09:34:27 -0500

    fix install.sh

--- install.sh
+++ install.sh
@@ -23,26 +23,29 @@ else
 	exit 1
 fi
 
-$i xorg 
-$i intel-microcode nvidia-driver
+$i xorg xbacklight xbindkeys xinput xorg-dev
+$i intel-microcode
 $i linux-headers-$(uname -r) build-essential make ninja-build fuse automake pkg-config cmake autoconf git python3-pip g++
-$i libncurses-dev flex bison openssl libssl-dev dkms libelf-dev pkg-config liblz4-tool bc rsync debhelper dwarves 
+$i libncurses-dev flex bison openssl libssl-dev dkms libelf-dev pkg-config liblz4-tool bc rsync debhelper dwarves
 $i curl wget
 $i unzip tar gzip
-$i pulseaudio pavucontrol 
+$i pulseaudio pavucontrol
+$i alsa-utils alsa-firmware-loaders alsa-oss alsa-tools alsamixergui volumeicon-alsa pacman paprefs pavumeter pulseaudio-utils sound-icons
+$i lame libdvdnav4 libfaac0 libmad0 libmp3lame0 libquicktime2 libstdc++5 libxvidcore4 twolame vorbis-tools x264
+$i gstreamer1.0-x gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-alsa gstreamer1.0-pulseaudio gstreamer1.0-tools
 $i chromium-l10n fonts-noto fonts-noto-cjk fonts-noto-extra fonts-noto-color-emoji
-$i kitty dunst libnotify-bin flameshot lxappearance feh ristretto tumbler gedit python3.11-venv ripgrep lxpolkit pcmanfm zathura htop ntfs-3g mpv
+$i kitty dunst libnotify-bin flameshot lxappearance feh ristretto tumbler gedit python3.11-venv ripgrep lxpolkit pcmanfm zathura htop ntfs-3g
 
 #openbox
-#$i openbox obconf 
+#$i openbox obconf
 
 # i3 gaps
-$i meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev i3status
+#$i meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev i3status
 
-cd
-git clone https://github.com/Airblader/i3 i3-gaps
-cd i3-gaps
-mkdir -p build && cd build
-meson --prefix /usr/local
-ninja
-$s ninja install
+#cd
+#git clone https://github.com/Airblader/i3 i3-gaps
+#cd i3-gaps
+#mkdir -p build && cd build
+#meson --prefix /usr/local
+#ninja
+#$s ninja install
